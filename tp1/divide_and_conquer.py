cc8f470825a5e5bde878afe1623d6545796de564
sid ali <sid-ali.guermache@polymtl.ca>
2021-03-03 18:48:45 -0500

    ajout algo brute force

--- tp1/divide_and_conquer.py
+++ tp1/divide_and_conquer.py
@@ -1,10 +1,13 @@
-import argparse
-import numpy as np
-import time
+
 
 
 # partie de code de l'algorithme divide_and_conquer inspire de https://www.learnbay.io/the-skyline-problem/
 # partie de code de merge inspire de
+import argparse
+import time
+
+import numpy as np
+
 
 def is_valid(self, result, new_height):
     return not result or result[-1][1] != new_height
@@ -68,33 +71,33 @@ def divide_and_conquer(buildings):
     return merge_buildings(left_buildings, right_buildings)
 
 
-def run(algo, path, print_time):
-    # 'C:/Users/Sid Ali/PycharmProjects/INF8775/tp1/N1000_0'
-    my_buildings = np.loadtxt(path, dtype=int, skiprows=1)
-    list_building = my_buildings.tolist()
-    # print(list_building)
-
-    if algo == 'recursif':
-        begin = time.time()
-        solution = divide_and_conquer(list_building)
-        end = time.time()
-
-    if print_time:
-        print((end - begin) * 1000)
-
-
-if __name__ == '__main__':
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--algo", \
-                        help="Représente le type d'algorithme", \
-                        action='store', required=True, metavar='NOM_ALGO', type=str)
-    parser.add_argument("-e", "--chemin", \
-                        help="Représente le chemin de l'exemplaires d'une même taille à tester", \
-                        action='store', required=True, metavar='CH_EXEMPLAIRES', type=str)
-    parser.add_argument("-t", "--time", \
-                        help="Représente le temps d'execution de l'algorithme", \
-                        action='store_true')
-
-    args = parser.parse_args()
-    run(args.algo, args.chemin, args.time)
+# def run(algo, path, print_time):
+#     # 'C:/Users/Sid Ali/PycharmProjects/INF8775/tp1/N1000_0'
+#     my_buildings = np.loadtxt(path, dtype=int, skiprows=1)
+#     list_building = my_buildings.tolist()
+#     # print(list_building)
+#
+#     if algo == 'recursif':
+#         begin = time.time()
+#         solution = divide_and_conquer(list_building)
+#         end = time.time()
+#
+#     if print_time:
+#         print((end - begin) * 1000)
+#
+#
+# if __name__ == '__main__':
+#     # Parse arguments
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-a", "--algo", \
+#                         help="Représente le type d'algorithme", \
+#                         action='store', required=True, metavar='NOM_ALGO', type=str)
+#     parser.add_argument("-e", "--chemin", \
+#                         help="Représente le chemin de l'exemplaires d'une même taille à tester", \
+#                         action='store', required=True, metavar='CH_EXEMPLAIRES', type=str)
+#     parser.add_argument("-t", "--time", \
+#                         help="Représente le temps d'execution de l'algorithme", \
+#                         action='store_true')
+#
+#     args = parser.parse_args()
+#     run(args.algo, args.chemin, args.time)
--- tp1/executable.py
+++ tp1/executable.py
@@ -1,3 +1,42 @@
-class Executable:
-    pass
+import argparse
+import numpy as np
+import time
+import divide_and_conquer as dc
+import brute as brute
 
+
+def run(algo, path, print_time):
+    # 'C:/Users/Sid Ali/PycharmProjects/INF8775/tp1/N1000_0'
+    my_buildings = np.loadtxt(path, dtype=int, skiprows=1)
+    list_building = my_buildings.tolist()
+    # print(list_building)
+
+    if algo == 'brute':
+        begin = time.time()
+        solution = brute.getSkyline(list_building)
+        end = time.time()
+
+    if algo == 'recursif':
+        begin = time.time()
+        solution = dc.divide_and_conquer(list_building)
+        end = time.time()
+
+    if print_time:
+        print((end - begin) * 1000)
+
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--algo", \
+                        help="Représente le type d'algorithme", \
+                        action='store', required=True, metavar='NOM_ALGO', type=str)
+    parser.add_argument("-e", "--chemin", \
+                        help="Représente le chemin de l'exemplaires d'une même taille à tester", \
+                        action='store', required=True, metavar='CH_EXEMPLAIRES', type=str)
+    parser.add_argument("-t", "--time", \
+                        help="Représente le temps d'execution de l'algorithme", \
+                        action='store_true')
+
+    args = parser.parse_args()
+    run(args.algo, args.chemin, args.time)
